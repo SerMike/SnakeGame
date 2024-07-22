7c9ec51be2ea911a46422c1443ca5c5fd11f6106
Michael Brana <SerMike@users.noreply.github.com>
2024-07-22 18:03:23 -0400

    added new methods to extend snake

--- snake.py
+++ snake.py
@@ -18,11 +18,17 @@ class Snake:
     def create_snake(self):
         # Create initial snake segments
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         # Head of snake moves forward, and rest of segments follow head's previous positions
